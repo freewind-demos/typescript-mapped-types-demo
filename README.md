4b1dfc45fd4844b817033b4e977a92c3c5310fcc
freewind <nowindlee@gmail.com>
2018-09-02 22:34:11 +0800

    demo

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ TypeScript Base Demo
 npm install
 npx ts-node hello.ts
 ```
+
+```
+{ name: 'Freewind', location: 'China' }
+```
+
+Resources
+---------
+
+- <https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html#mapped-types>
--- hello.ts
+++ hello.ts
@@ -1,1 +1,18 @@
-console.log("Hello, Typescript");
+interface Person {
+    name: string;
+    age: number;
+    location: string;
+}
+
+type PartialOf<T> = {
+    [P in keyof T]?: T[P]
+}
+
+type PartialPerson = PartialOf<Person>
+
+const person: PartialPerson = {
+    name: 'Freewind',
+    location: 'China'
+}
+
+console.log(person)
